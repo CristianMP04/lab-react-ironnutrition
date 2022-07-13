d8aa40aac816ac49db854fe306076dc1f85d6409
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-13 19:30:39 +0200

    final ite 4
    final ite 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import foods from './foods.json';
-// import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import {useState} from "react";
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   let [comidas, setComidas] = useState(foods)
+
+  const addNewFood = (newFood) => {
+    const updatedFood = [...comidas, newFood];
+    setComidas(updatedFood);
+  };
+
   return <div className="App">
       {comidas.map((comida) => {
-          return <FoodBox name = {comida.name} calories  = {comida.calories} image = {comida.image} servings = {comida.servings} />
+          return (<FoodBox name = {comida.name} calories  = {comida.calories} image = {comida.image} servings = {comida.servings}/>)
+           
         })
+        
       }
+      <AddFoodForm AddFoodForm= {addNewFood} />
   </div>;
 }
 export default App;
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Divider, Input } from 'antd';
+
+function AddFoodForm(props) {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState(0);
+    const [servings, setServings] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newFood = {
+          name: name,
+          image: image,
+          calories: calories,
+          servings: servings
+        };
+    
+        console.log("Submitted", newFood);
+        props.AddFoodForm(newFood);
+    
+        // Reset the state
+        setName("");
+        setImage("");
+        setCalories(0);
+        setServings(0);
+      };
+
+
+    return (
+        <form on onSubmit={handleSubmit}>
+          <Divider>Add Food Entry</Divider>
+    
+          <label>Name</label>
+          <Input value={name} type="text" onChange={(e) => setName(e.target.value)} />
+    
+          <label>Image</label>
+          <Input value={image} type="text" onChange={(e) => setImage(e.target.value)} />
+    
+          <label>Calories</label>
+          <Input value={calories} type="text" onChange={(e) => setCalories(e.target.value)} />
+    
+          <label>Servings</label>
+          <Input value={servings} type="text" onChange={(e) => setServings(e.target.value)} />
+    
+          <button type="submit">Create</button>
+        </form>
+      );
+
+}
+
+export default AddFoodForm;
